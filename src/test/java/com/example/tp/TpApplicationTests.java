bc6606e2939dd91108b0e1070e767b2cfbb0b3b4
Souad Malak Lamrizegar <souad.lamrizegar@univ-constantine2.dz>
2025-11-19 09:19:50 +0100

    Fix GitHub Actions workflow for master branch and update test config

--- src/test/java/com/example/tp/TpApplicationTests.java
+++ src/test/java/com/example/tp/TpApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.tp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
+
 class TpApplicationTests {
 
 	@Test
